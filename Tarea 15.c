cfab96d97b00b5db04093a5a0f6c6f6ae7da4a34
raygadasbaezrodrigo <31478555+raygadasbaezrodrigo@users.noreply.github.com>
2017-10-29 18:37:14 -0600

    Add files via upload

---
+++ Tarea 15.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main ()
+{
+
+int var1=5,var2=2,resultado=0;
+	
+	//suma
+	resultado=var1 + var2;
+	printf("%d + %d = %d\n",var1,var2,resultado);
+	
+	
+	//resta
+	resultado=var1 - var2;
+	printf("%d - %d = %d\n",var1,var2,resultado);
+
+	//multiplicacion
+	resultado=var1 * var2;
+	printf("%d * %d = %d\n",var1,var2,resultado);
+
+	//division
+	
+	resultado=var1 / var2;
+		printf("%d / %d = %d\n",var1,var2,resultado);
+	
+	//modulo		
+	resultado=var1 % var2;
+	printf("%d mod %d = %d\n",var1,var2,resultado);
+
+
+
+
+}
